330c289edcee979a8241b6be8f5840b5c77da393
Mogusguy <antonsalt1231@gmail.com>
2025-07-18 17:24:15 +0100

    Changed some things

--- src/main/java/net/mogusguy/circuitry/Circuitry.java
+++ src/main/java/net/mogusguy/circuitry/Circuitry.java
@@ -1,5 +1,7 @@
 package net.mogusguy.circuitry;
 
+import net.minecraft.world.item.CreativeModeTabs;
+import net.mogusguy.circuitry.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -34,6 +36,8 @@ public class Circuitry {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
@@ -46,7 +50,9 @@ public class Circuitry {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.ENDERITE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/mogusguy/circuitry/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.mogusguy.circuitry.item;
+
+import net.minecraft.world.item.Item;
+import net.mogusguy.circuitry.Circuitry;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Circuitry.MOD_ID);
+
+    public static final DeferredItem<Item> ENDERITE = ITEMS.register("enderite",
+            () -> new Item(new Item.Properties()));
+
+
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
